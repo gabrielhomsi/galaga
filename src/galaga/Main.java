40eff855859ab6c2e9feb5114a69daa2d5fea10f
Bruno Gonçalves <brunodg.dev@gmail.com>
2015-10-09 10:46:07 -0300

    Testes iniciais com o Java2D sendo feitos. Classe Teste Criada.

--- src/galaga/Main.java
+++ src/galaga/Main.java
@@ -1,9 +1,27 @@
 package galaga;
 
 import javax.swing.*;
+import java.awt.*;
+
+class Teste extends JPanel {
+
+    private void doDrawing(Graphics g) {
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.drawString("Galaga", 100, 100);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        doDrawing(g);
+    }
+
+}
 
 public class Main extends JFrame {
+
     Main() {
+        add(new Teste());
         this.setTitle("Galaga");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
