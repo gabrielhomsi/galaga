6dd94239727a3c411e25c482785addbc9bddf7f5
maupaso <maupaso@gmail.com>
2015-10-28 17:06:26 -0200

    Player Colorido

--- src/galaga/shared/gameobjects/Craft.java
+++ src/galaga/shared/gameobjects/Craft.java
@@ -161,7 +161,11 @@ public class Craft implements GameObject {
 
     @Override
     public String getImagePath() {
-        return "assets/playerShip1_Blue.png";
+        if(this.connectionId %2 == 0){
+            return "assets/playerShip1_Blue.png";
+        } else{
+            return "assets/playerShip2_Green.png";
+        }
     }
 
     //ricardo--------------------------------------------------
